7e3f4ae5e974891ddd23a1362c049939e78e1730
Dr-mkelvo <kelvincent002@gmail.com>
2023-01-25 10:38:14 -0800

    function.c file

---
+++ functions.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+
+/**
+ * print_char - Prints a char
+ *
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
