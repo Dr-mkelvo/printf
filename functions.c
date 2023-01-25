1a8ba3b0a413e73546dd2ac96bf36200d49894f7
Dr-mkelvo <kelvincent002@gmail.com>
2023-01-25 10:41:30 -0800

    Printing a string comments for function.c file

--- functions.c
+++ functions.c
@@ -20,3 +20,15 @@ int print_char(va_list types, char buffer[],
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
+
+/************************* PRINT A STRING *************************/
+/**
+ * print_string - Prints a string
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
