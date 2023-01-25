c106ad5ca90a911d05db2829ad80715aa33fb026
Dr-mkelvo <kelvincent002@gmail.com>
2023-01-25 10:49:19 -0800

    func1.c file

---
+++ function1.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/************************* PRINT UNSIGNED NUMBER *************************/
+/**
+ * print_unsigned - Prints an unsigned number
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed.
+ */
