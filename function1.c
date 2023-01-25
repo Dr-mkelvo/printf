38bc67544785c1368edbbf99c0117c7772921a6c
Dr-mkelvo <kelvincent002@gmail.com>
2023-01-25 10:50:53 -0800

    PRINT UNSIGNED NUMBER IN OCTAL comments

--- function1.c
+++ function1.c
@@ -34,3 +34,14 @@ int print_unsigned(va_list types, char buffer[],
 
 	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
+/************* PRINT UNSIGNED NUMBER IN OCTAL  ****************/
+/**
+ * print_octal - Prints an unsigned number in octal notation
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
