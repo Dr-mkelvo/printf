aa9f5b79524a94b204230b395d0ff91a2d858f11
Dr-mkelvo <kelvincent002@gmail.com>
2023-01-25 11:10:28 -0800

    Function 2

---
+++ functions2.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/****************** PRINT POINTER ******************/
+/**
+ * print_pointer - Prints the value of a pointer variable
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed.
+ */
