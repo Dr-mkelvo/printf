628cb48eb2563bf28f35309e62b44731f28d676f
Dr-mkelvo <kelvincent002@gmail.com>
2023-01-25 10:40:30 -0800

    function.c file buffer, types, flag

--- functions.c
+++ functions.c
@@ -13,3 +13,10 @@
  * @size: Size specifier
  * Return: Number of chars printed
  */
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char c = va_arg(types, int);
+
+	return (handle_write_char(c, buffer, flags, width, precision, size));
+}
