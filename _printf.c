685a6c4761f048ae23f05f2141d065f91612c518
Dr-mkelvo <kelvincent002@gmail.com>
2023-01-25 10:33:50 -0800

    printf.c file update 2

--- _printf.c
+++ _printf.c
@@ -30,3 +30,19 @@ int _printf(const char *format, ...)
 			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
